f11f3d50dc4d98b4e4ee582c8303abdc50985cc7
triskadecaepyon <dcltechnology@gmail.com>
2017-04-28 16:44:18 -0500

    Changes that reconfigure queue to save-only (refactor)

--- telem_main.py
+++ telem_main.py
@@ -4,29 +4,65 @@ import time
 import numpy as np
 
 
-GLOBAL_WINDOW_SIZE = 100
+GLOBAL_WINDOW_SIZE = 1000
 
-
-def store_data(queue):
+def data_read_daemon(queue, ringbuf):
     """
-    Storage process, used to take data off the queue from
-    sensor reads and store it to short term HDF5.
+    Data reading process (psuedo-daemon)
+    Fills up window data, then triggers
+    window analysis process
     """
-    time.sleep(0.25) # Simulating data delay (temporary)
-    data = queue.get()
-    # print('Saving', data)
+    window_counter = GLOBAL_WINDOW_SIZE
+    data_counter = 0
+    print("Started Sensor Read Daemon")
+
+    while True:
+        # Expect some of the communications to be VERY slow
+        time.sleep(np.random.random()*0.01)
+        data = np.random.randint(0, 100) # Random data (temporary)
+
+        # print(data)
+        ringbuf.extend(np.array([data])) # place data in the ring buffer
+
+        window_counter -= 1 # Decrement local window size counter
+        if window_counter == 0:
+            # Process the data currently in the ring buffer
+            ring_data = ringbuf.get()
+            data_counter += 1
+            print("Buffer filled")
+            # print(ring_data)
+            window_counter = GLOBAL_WINDOW_SIZE
+            # Send off to window process function
+            pw = Process(target=process_window_data,
+                         args=(ring_data, data_counter, queue))
+            pw.start()
 
 
-def process_window_data(data, data_count):
+def process_window_data(data, data_count, queue):
     """
     Process for saving the windowed timeseries data
-    for analysis via metrics or streaming algorithms
-    Not meant for HDF5, but for feeding NumPy arrays
-    to other functions or processes.
+    for analysis via metrics or streaming algorithms.
+    Places data on queue for long-term storage
     """
-    time.sleep(5) # Simulating data send delay (temporary)
+    time.sleep(3) # Simulating data send delay (temporary)
     print("large data read, saving...", data_count)
+    queue.put(data) # placing read data in the queue
 
+
+def store_window_data(queue):
+    """
+    Process for Saving the data in long-term storage
+    """
+    while True:
+        if queue.empty() is True:
+            pass # nothing to do
+        else:
+            if queue.full() is True:
+                print("WARNING: QUEUE IS FULL")
+            print("taking off queue")
+            data = queue.get()
+            time.sleep(5) # Simulating data send delay (temporary)
+            print("Long term storage done")
 
 if __name__ == '__main__':
     """
@@ -37,37 +73,18 @@ if __name__ == '__main__':
     of time series data to process in algorithms or streaming
     machine learning algorithms.
     """
+    # Main shared Queue and Ring Buffer
     q = Queue()
     ringbuf = telem_tools.RingBuffer(GLOBAL_WINDOW_SIZE)
-    window_counter = GLOBAL_WINDOW_SIZE
-    data_counter = 0
+
+    # Start data read daemon
+    dr = Process(target=data_read_daemon, args=(q,ringbuf))
+    dr.start()
+
+    # Start write daemon
+    wr = Process(target=store_window_data, args=(q,))
+    wr.start()
 
     while True:
-        # time.sleep(0.1)
-        # This is the data read process; simulating when data is ready
-        if (np.random.randint(0,2)):
-            data = np.random.randint(0, 100) # Random data (temporary)
-            # print(data)
-            q.put(data) # placing read data in the queue
-            ringbuf.extend(np.array([data])) # place data in the ring buffer
-
-            window_counter -= 1 # Decrement local window size counter
-            if window_counter == 0:
-                # Process the data currently in the ring buffer
-                ring_data = ringbuf.get()
-                data_counter += 1
-                print(ring_data)
-                window_counter = GLOBAL_WINDOW_SIZE
-                # Send off to window process function
-                pw = Process(target=process_window_data,
-                             args=(ring_data, data_counter))
-                pw.start()
-
-            # Longer storage process to HDF5
-            s = Process(target=store_data, args=(q,))
-            s.start() # Don't need join, non-blocking for better response
-
-        else:
-            pass
-            # print("Waiting")
-            # We don't use join as to not block the calculation
+        time.sleep(0.1)
+        # This is the place where your main loop or logic goes
